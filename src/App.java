b0f8eaf6b0d6459e60913d3bb66d0050d4767a74
mehditag <mehdiitaguema@gmail.com>
2023-12-04 15:06:11 +0100

    ""

--- src/App.java
+++ src/App.java
@@ -6,20 +6,17 @@ public class App {
         //Démarrage d'un Thread
         System.out.println("######### Démarrage d'un Thread ########");
         System.out.println(Thread.currentThread().getName());
-        for (int i=0; i<5; i++){
+        /*for (int i=0; i<5; i++){
             Thread thread = new Thread(() -> System.out.println(Thread.currentThread().getName()));
             thread.start();
-        }
+        }*/
 
 
         //Mise en pause d'un Thread
         System.out.println("######### Mise en pause d'un Thread ###########");
-        System.out.println("Thread started"+Thread.currentThread().getName());
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i=0; i<5; i++){
+            Thread thread = new Thread(new Task();
+            thread.start();
         }
-        System.out.println("Thread finished"+Thread.currentThread().getName());
     }
 }
